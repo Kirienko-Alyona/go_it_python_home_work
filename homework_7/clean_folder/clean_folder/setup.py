b32971a54470a7fa122c21ad05a6e175f8cc7026
Kirienko-Alyona <112887968+Kirienko-Alyona@users.noreply.github.com>
2022-10-27 16:08:26 +0300

    Delete homework_7/clean_folder/clean_folder directory

--- homework_7/clean_folder/clean_folder/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='clean_folder',
-    version='1',
-    description='Sorted files for extensions',
-    packages=find_packages(),
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:start_terminal']}
-    )
